e7fe04bc98e1463e73d1a2d6c820900320187f6f
Michael Frenkel <michael.frenkel@outlook.com>
2019-11-29 19:27:23 -0800

    add Meta component and some styles

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Meta from "./Meta";
+import styled from "styled-components";
+
 class Page extends Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
         {this.props.children}
       </div>

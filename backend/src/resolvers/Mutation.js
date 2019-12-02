ec9934fdcd019a2b331d66c08009f0967f34beb0
Michael Frenkel <michael.frenkel@outlook.com>
2019-12-01 20:27:52 -0800

    lint and format

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,14 +1,11 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
 
-    const item = await ctx.db.mutation.createItem(
-      {
+    const item = await ctx.db.mutation.createItem({
         data: {
           ...args
         }
-      },
-      info
-    );
+      },info);
     console.log(item);
     return item;
   }

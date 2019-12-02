4ec866fefd0f3543c3b6335c940ee8c7674269a5
Michael Frenkel <michael.frenkel@outlook.com>
2019-12-01 20:26:26 -0800

    add eslint config

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,5 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
 
     const item = await ctx.db.mutation.createItem(
       {
@@ -10,9 +9,7 @@ const Mutations = {
       },
       info
     );
-
     console.log(item);
-
     return item;
   }
   // createDog(parent, args, ctx, info) {

7deed8fcb9cb53326efa567640820cd8ab00b538
Michael Frenkel <michael.frenkel@outlook.com>
2019-12-10 20:51:32 -0800

    update schema and add update mutation

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,13 +1,13 @@
-const { forwardTo } = require("prisma-binding");
+const { forwardTo } = require('prisma-binding');
 
 const Query = {
-  items: forwardTo("db")
-
+  items: forwardTo('db'),
+  item: forwardTo('db'),
   // async items(parent, args, ctx, info) {
-  //   console.log("Getting Items!!");
+  //   console.log('Getting Items!!');
   //   const items = await ctx.db.query.items();
   //   return items;
-  // }
+  // },
 };
 
 module.exports = Query;

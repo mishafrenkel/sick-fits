028a5b761fbe93d4194d4f7c68010d53b5d7079b
Michael Frenkel <michael_frenkel@gap.com>
2019-12-26 20:18:46 -0800

    implement admin permissions dashboard

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config({ path: 'variables.env' });
-const createServer = require('./createServer');
-const db = require('./db');
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
@@ -20,6 +20,19 @@ server.express.use((req, res, next) => {
   next();
 });
 
+// 2. Create a middleware that populates the user on each request
+
+server.express.use(async (req, res, next) => {
+  // if they aren't logged in, skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    "{ id, permissions, email, name }",
+  );
+  req.user = user;
+  next();
+});
+
 server.start(
   {
     cors: {
@@ -29,5 +42,5 @@ server.start(
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
-  }
+  },
 );
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,9 +1,10 @@
-const { forwardTo } = require('prisma-binding');
+const { forwardTo } = require("prisma-binding");
+const { hasPermission } = require("../utils");
 
 const Query = {
-  items: forwardTo('db'),
-  item: forwardTo('db'),
-  itemsConnection: forwardTo('db'),
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
   me(parent, args, ctx, info) {
     // check if there is a current user ID
     if (!ctx.request.userId) {
@@ -13,8 +14,20 @@ const Query = {
       {
         where: { id: ctx.request.userId },
       },
-      info
+      info,
     );
+  },
+  async users(parent, args, ctx, info) {
+    // 1. Check if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in!");
+    }
+    console.log(ctx.request.userId);
+    // 2. Check if the user has the permissions to query all the users
+    hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
+
+    // 2. if they do, query all the users!
+    return ctx.db.query.users({}, info);
   },
 };
 

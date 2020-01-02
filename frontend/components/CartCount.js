a20738a19fed3dfb57a9be005111013d06efddef
Michael Frenkel <michael_frenkel@gap.com>
2020-01-01 19:50:35 -0800

    clean up render prop mess with react-adopt package

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import styled from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import styled from "styled-components";
 
 const AnimationStyles = styled.span`
   position: relative;
@@ -37,7 +37,7 @@ const Dot = styled.div`
   min-width: 3rem;
   margin-left: 1rem;
   font-weight: 100;
-  font-feature-settings: 'tnum';
+  font-feature-settings: "tnum";
   font-variant-numeric: tabular-nums;
 `;
 
